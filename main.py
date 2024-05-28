915f5f1668e9f021719e2b4edd44355aa6062235
wen <z0916945857@gmail.com>
2024-05-28 08:28:58 +0000

    db

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ dbpassword = 'mysql'
 dbhost = 'localhost'
 dbname = 'mydb'
 
-cnx = mysql.connector.connect(user=dbuser, password=dbpassword, host=dbhost, database=dbname)
+cnx = mysql.connector.connect(user=dbuser, password=dbpassword, host=dbhost)
 with cnx.cursor() as cursor:
+    cursor.execute(f'CREATE DATABASE IF NOT EXISTS {dbname} ;')
+    cursor.execute(f'USE {dbname};')
 
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS test1 (
+    column1 int
+);''')
+    
+    
     result = cursor.execute(f'''
 SELECT table_name FROM information_schema.tables
 WHERE table_schema = '{dbname}';
